dd7f81ba02cbe37cf8546ef3d483bb1385d8edea
Eshita Jain <eshitajain555@gmail.com>
2024-03-19 00:08:43 +0530

    added pdf

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -9,6 +9,6 @@ router.post("/signup", auth.signup);
 router.post("/login", auth.login);
 router.get("/upload", middleware,upload.GetUploadURL);
 router.post("/uploadfile", middleware, upload.UploadFile);
-
+router.get("/download/:id", middleware, upload.DownloadFile);
 
 module.exports= router;
